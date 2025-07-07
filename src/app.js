49c703101e5fb3b1922e0dd1779adebdfbada413
Amin <70637820+amin-newcastle@users.noreply.github.com>
2025-07-07 14:07:49 +0100

    MPRO-0000: Fix JWT and multiple env

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
 });
 
 // Middleware:
-
 // Parses JSON bodies and attaches the result to req.body
 app.use(bodyParser.json());
 // Global error handler for HTTP request/response
@@ -20,7 +19,7 @@ app.use((error, req, res, next) => {
   res.status(500).json({ message: error.message });
 });
 
-// Routes (entry point into the Controller layer)
+// Mount Routes (entry point into the Controller layer)
 app.use('/api/v1/auth', authRoutes);
 
 // Export the configured app instance (to be used in the main server file or tests)
